fa47503a6804c914ad82c5addd1533bddacd2f1d
Stephanie <muamua124@gmail.com>
2023-01-01 13:32:19 +0800

    feat：update format

--- src/main/webapp/memberCenter/pages/sideBar.js
+++ src/main/webapp/memberCenter/pages/sideBar.js
@@ -1,27 +1,34 @@
-$("#profile").on("click",function(){
+// $("#profile").on("click",function(){
 
-    window.location.href="./accountBasicInfo.html?memId="+sessionStorage.getItem("memId");
-})
+//     window.location.href="./accountBasicInfo.html?memId="+sessionStorage.getItem("memId");
+// })
 
-$.ajax({
+$(window).on("load", () => {
+  loadMemberInfo();
+});
+
+const loadMemberInfo = () => {
+  $.ajax({
     url: "http://localhost:8080/oladesign/member",
     type: "GET",
     data: { memId: sessionStorage.getItem("memId") },
     dataType: "json", // 預期會接收到回傳資料的格式： json | xml | html
     success: function (data) {
       console.log(data.memName);
-      console.log("success");
-  
+      console.log("success to get memName");
+
       let list_html = `
       <p>${data.memName}</p>
       <a href="#"><i class="fa fa-circle text-success"></i> 線上</a>
           `;
       $("#memberName").html(list_html);
-  
-    
+
+      let list_html2 = `<img src="${data.memPhotoBase64}" class="img-circle" alt="User Image"> `;
+      $("div.image").html(list_html2);
     },
     error: function (xhr) {
       console.log("error");
       console.log(xhr);
     },
-});
+  });
+};
